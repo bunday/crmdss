f7e54974e6b80bdbbeaa68bcddbca1b5179b42f2
Bundayy <seyyids90@gmail.com>
2018-01-13 00:01:33 +0100

    Model Update

--- database/migrations/2018_01_12_224613_create_feedback_decisions_table.php
+++ database/migrations/2018_01_12_224613_create_feedback_decisions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateFeedbackDecisionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('feedback_decisions', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('fname');
-            $table->string('lname');
-            $table->string('email')->unique();
-            $table->string('phone');
-            $table->string('password');
-            $table->string('category');
-            $table->rememberToken();
+            $table->integer('fid');
+            $table->integer('did');
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('feedback_decisions');
     }
 }
